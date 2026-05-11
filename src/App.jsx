043246ff84371ad6168b56150b58d282a97c5bc9
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-05-11 17:11:10 +0200

    Add movie IDs and implement genre filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 function App() {
 
   const initialMovies = [
-    { title: 'Inception', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZjhkNjM0ZTMtNGM5MC00ZTQ3LTk3YmYtZTkzYzdiNWE0ZTA2XkEyXkFqcGc@._V1_.jpg' },
-    { title: 'Il Padrino', genre: 'Thriller', img: 'https://erigibbi.wordpress.com/wp-content/uploads/2017/04/il-padrino.jpg?w=1075' },
-    { title: 'Titanic', genre: 'Romantico', img: 'https://m.media-amazon.com/images/I/811lT7khIrL._AC_UF894,1000_QL80_.jpg' },
-    { title: 'Batman', genre: 'Azione', img: 'https://pad.mymovies.it/filmclub/2007/02/131/locandina.jpg' },
-    { title: 'Interstellar', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZmVlOWIzODktNTMxNC00NGRlLTk0NDQtNWM5MGE2OGUxOWRkXkEyXkFqcGc@._V1_.jpg' },
-    { title: 'Pulp Fiction', genre: 'Thriller', img: 'https://m.media-amazon.com/images/I/81UTs3sC5hL._AC_UF894,1000_QL80_.jpg' },
+    { id: 1, title: 'Inception', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZjhkNjM0ZTMtNGM5MC00ZTQ3LTk3YmYtZTkzYzdiNWE0ZTA2XkEyXkFqcGc@._V1_.jpg' },
+    { id: 2, title: 'Il Padrino', genre: 'Thriller', img: 'https://erigibbi.wordpress.com/wp-content/uploads/2017/04/il-padrino.jpg?w=1075' },
+    { id: 3, title: 'Titanic', genre: 'Romantico', img: 'https://m.media-amazon.com/images/I/811lT7khIrL._AC_UF894,1000_QL80_.jpg' },
+    { id: 4, title: 'Batman', genre: 'Azione', img: 'https://pad.mymovies.it/filmclub/2007/02/131/locandina.jpg' },
+    { id: 5, title: 'Interstellar', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZmVlOWIzODktNTMxNC00NGRlLTk0NDQtNWM5MGE2OGUxOWRkXkEyXkFqcGc@._V1_.jpg' },
+    { id: 6, title: 'Pulp Fiction', genre: 'Thriller', img: 'https://m.media-amazon.com/images/I/81UTs3sC5hL._AC_UF894,1000_QL80_.jpg' },
   ]
 
   const [movies, setMovies] = useState(initialMovies)
+  const [selectGenre, setSelectGenre] = useState('')
+
+  useEffect(() => {
+    if (selectGenre === '') {
+      setMovies(initialMovies)
+    } else {
+      const filteredMovies = initialMovies.filter(movie => movie.genre === selectGenre)
+      setMovies(filteredMovies)
+    }
+  }, [selectGenre])
+
+  function getMoviesFiltered(e) {
+
+    setSelectGenre(e.target.value)
+  }
 
 
 
@@ -20,8 +35,8 @@ function App() {
     <>
       <h1>Movies</h1>
 
-      <select class="form-select">
-        <option value='' selected>Tutti i generi</option>
+      <select className="form-select" id="select-genre" value={selectGenre} onChange={getMoviesFiltered}>
+        <option value=''>Tutti i generi</option>
         <option value="Fantascienza">Fantascienza</option>
         <option value="Thriller">Thriller</option>
         <option value="Romantico">Romantico</option>
@@ -34,7 +49,7 @@ function App() {
 
           {
             movies.map(movie => (
-              <div className="col 12 col-sm-6 col-md-4">
+              <div className="col 12 col-sm-6 col-md-4" key={movie.id}>
                 <div className="card h-100">
                   <img src={movie.img} className="film-img " alt="" />
                   <div className="card-body">
