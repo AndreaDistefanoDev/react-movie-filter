31ea043acfbd59c6a7fcaf217e8523d78c52ef5d
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-05-11 16:33:48 +0200

    Enhance movie display with images and add genre filter dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { useState } from "react"
 function App() {
 
   const initialMovies = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
+    { title: 'Inception', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZjhkNjM0ZTMtNGM5MC00ZTQ3LTk3YmYtZTkzYzdiNWE0ZTA2XkEyXkFqcGc@._V1_.jpg' },
+    { title: 'Il Padrino', genre: 'Thriller', img: 'https://erigibbi.wordpress.com/wp-content/uploads/2017/04/il-padrino.jpg?w=1075' },
+    { title: 'Titanic', genre: 'Romantico', img: 'https://m.media-amazon.com/images/I/811lT7khIrL._AC_UF894,1000_QL80_.jpg' },
+    { title: 'Batman', genre: 'Azione', img: 'https://pad.mymovies.it/filmclub/2007/02/131/locandina.jpg' },
+    { title: 'Interstellar', genre: 'Fantascienza', img: 'https://m.media-amazon.com/images/M/MV5BZmVlOWIzODktNTMxNC00NGRlLTk0NDQtNWM5MGE2OGUxOWRkXkEyXkFqcGc@._V1_.jpg' },
+    { title: 'Pulp Fiction', genre: 'Thriller', img: 'https://m.media-amazon.com/images/I/81UTs3sC5hL._AC_UF894,1000_QL80_.jpg' },
   ]
 
   const [movies, setMovies] = useState(initialMovies)
@@ -20,18 +20,33 @@ function App() {
     <>
       <h1>Movies</h1>
 
+      <select class="form-select">
+        <option value='' selected>Tutti i generi</option>
+        <option value="Fantascienza">Fantascienza</option>
+        <option value="Thriller">Thriller</option>
+        <option value="Romantico">Romantico</option>
+        <option value="Azione">Azione</option>
+
+      </select>
+
       <div className="container">
         <div className="row">
 
           {
             movies.map(movie => (
-              <div className="col">
-                <div>
-                  <strong>Titolo: </strong>{movie.title}
+              <div className="col 12 col-sm-6 col-md-4">
+                <div className="card h-100">
+                  <img src={movie.img} className="film-img " alt="" />
+                  <div className="card-body">
+                    <div>
+                      <strong>Titolo: </strong>{movie.title}
+                    </div>
+                    <div>
+                      <strong>Genere: </strong>{movie.genre}
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <strong>Genere: </strong>{movie.genre}
-                </div>
+
               </div>
 
             ))
